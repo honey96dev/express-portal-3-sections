ff1820358b091afd9304e007a13042ce9e2c5695
honey96dev <honey96dev@gmail.com>
2019-09-16 22:34:28 -0800

    human section is done.

--- routes/assets-api.js
+++ routes/assets-api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mediaSliderRouter from './assets-api/mediaSlider';
 import ourServicesRouter from './assets-api/ourServices';
 import businessPartnerRouter from './assets-api/businessPartner';
+import ourClientsRouter from './assets-api/ourClients';
 import directorBoardRouter from './assets-api/directorBoard';
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 router.use('/media-slider', mediaSliderRouter);
 router.use('/our-services', ourServicesRouter);
 router.use('/business-partner', businessPartnerRouter);
+router.use('/our-clients', ourClientsRouter);
 router.use('/director-board', directorBoardRouter);
 
 export default router;
