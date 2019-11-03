104885fee7392d657d8ac1c3731b51b5973d7024
honey96dev <honey96dev@gmail.com>
2019-11-03 00:15:27 -0800

    Update _config.prod.js

--- core/_config.prod.js
+++ core/_config.prod.js
@@ -7,7 +7,7 @@ const mysql = {
   connectionLimit: 10,
   host: '127.0.0.1',
   user: 'root',
-  password: '',
+  password: 'mohammed@123456',
   database: 'portal3sections',
   port: 3306,
   connectTimeout: 15000,
