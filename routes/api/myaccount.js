20c8b2e2089f2670ce8bdc02d78ffa1dd1ca6ec0
honey96dev <honey96dev@gmail.com>
2019-11-07 21:30:48 -0800

    2nd upload

--- routes/api/myaccount.js
+++ routes/api/myaccount.js
@@ -4,6 +4,7 @@ import db from '../../core/db';
 import {dbTblName} from "../../core/config";
 import tracer from "../../core/tracer";
 import strings from "../../core/strings";
+import myCrypto from "../../core/myCrypto";
 
 const router = express.Router();
 
@@ -15,7 +16,6 @@ const saveProc = async (req, res, next) => {
     [id, email, firstName, lastName, '', company, position, country, city, phone, 1],
   ];
   let sql = sprintf("INSERT INTO `%s` VALUES ? ON DUPLICATE KEY UPDATE `firstName` = VALUES(`firstName`), `lastName` = VALUES(`lastName`), `company` = VALUES(`company`), `position` = VALUES(`position`), `country` = VALUES(`country`), `city` = VALUES(`city`), `email` = VALUES(`email`), `phone` = VALUES(`phone`);", dbTblName.users);
-  tracer.info(sql);
   try {
     await db.query(sql, [rows]);
     res.status(200).send({
@@ -33,6 +33,48 @@ const saveProc = async (req, res, next) => {
   }
 };
 
+const changePasswordProc = async (req, res, next) => {
+  const language = req.get('language');
+  const langs = strings[language];
+  const {id, currentPassword, password} = req.body;
+  const currentHash = myCrypto.hmacHex(currentPassword);
+  const hash = myCrypto.hmacHex(password);
+  let sql = sprintf("SELECT * FROM `%s` WHERE `id` = '%s';", dbTblName.users, id);
+  try {
+    let rows = await db.query(sql, null);
+    if (rows.length === 0) {
+      res.status(200).send({
+        result: langs.error,
+        message: langs.notFound,
+      });
+      return;
+    }
+    const row = rows[0];
+    if (row['hash'] !== currentHash) {
+      res.status(200).send({
+        result: langs.error,
+        message: langs.currentPasswordIncorrect,
+      });
+      return;
+    }
+    sql = sprintf("UPDATE `%s` SET `hash` = '%s' WHERE `id` = '%s';", dbTblName.users, hash, id);
+    await db.query(sql, null);
+    res.status(200).send({
+      result: langs.success,
+      message: langs.successfullyChanged,
+    });
+  } catch (err) {
+    tracer.error(JSON.stringify(err));
+    tracer.error(__filename);
+    res.status(200).send({
+      result: langs.error,
+      message: langs.unknownServerError,
+      err,
+    });
+  }
+};
+
 router.post('/save', saveProc);
+router.post('/change-password', changePasswordProc);
 
 export default router;
